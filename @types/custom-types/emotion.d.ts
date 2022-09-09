bf4825ec049f1d2c2da732dedba01141063fbde2
HaJunRyu <hjr4557@naver.com>
2022-09-10 03:15:15 +0900

    Feat: 접근성 관련 mixin을 emotion theme에 추가한다

--- @types/custom-types/emotion.d.ts
+++ @types/custom-types/emotion.d.ts
@@ -2,11 +2,13 @@ import '@emotion/react';
 import { ColorsTheme } from '@/styles/themes/colors';
 import { FontsTheme } from '@/styles/themes/fonts';
 import { BreakPointTheme } from '@/styles/themes/breakPoint';
+import { A11yTheme } from '@/styles/themes/a11y';
 
 declare module '@emotion/react' {
   export interface Theme {
     colors: ColorsTheme;
     fonts: FontsTheme;
     breakPoint: BreakPointTheme;
+    a11y: A11yTheme;
   }
 }
--- src/styles/themes/index.ts
+++ src/styles/themes/index.ts
@@ -1,7 +1,8 @@
 import { colors } from './colors';
 import { fonts } from './fonts';
 import { breakPoint } from './breakPoint';
+import { a11y } from './a11y';
 
-const themes = { colors, fonts, breakPoint };
+const themes = { colors, fonts, breakPoint, a11y };
 
 export default themes;
