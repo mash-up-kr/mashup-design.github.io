d748f2c62b703d39967c21524c178176f972c82d
HaJunRyu <hjr4557@naver.com>
2022-09-10 03:36:35 +0900

    Feat: Z값 조정이 필요한 요소를 위해 zIdex를 emotion theme에 추가한다

--- @types/custom-types/emotion.d.ts
+++ @types/custom-types/emotion.d.ts
@@ -3,6 +3,7 @@ import { ColorsTheme } from '@/styles/themes/colors';
 import { FontsTheme } from '@/styles/themes/fonts';
 import { BreakPointTheme } from '@/styles/themes/breakPoint';
 import { A11yTheme } from '@/styles/themes/a11y';
+import { ZIndexTheme } from '@/styles/themes/zIndex';
 
 declare module '@emotion/react' {
   export interface Theme {
@@ -10,5 +11,6 @@ declare module '@emotion/react' {
     fonts: FontsTheme;
     breakPoint: BreakPointTheme;
     a11y: A11yTheme;
+    zIndex: ZIndexTheme;
   }
 }
---
+++ src/styles/themes/zIndex.ts
@@ -0,0 +1,8 @@
+export const zIndex = {
+  default: 0,
+  gnb: 5,
+  dropdown: 10,
+  gnbMenu: 15,
+};
+
+export type ZIndexTheme = typeof zIndex;
