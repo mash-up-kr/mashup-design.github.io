0b6de901757146a4609d7d0410a541dbf116b449
HaJunRyu <hjr4557@naver.com>
2022-09-10 17:42:18 +0900

    Feat: Footer가 항상 페이지의 최하단에 위치하게끔 한다

--- src/components/common/Layout/Layout.styled.ts
+++ src/components/common/Layout/Layout.styled.ts
@@ -3,12 +3,19 @@ import styled from '@emotion/styled';
 
 export const Layout = styled.div`
   ${({ theme }) => css`
+    display: flex;
+    flex-flow: column nowrap;
     margin: 0 auto;
     padding: 8rem 2.4rem 0;
     width: 100%;
+    min-height: 100vh;
     max-width: 120rem;
 
     @media (max-width: ${theme.breakPoint.media.tablet}) {
     }
   `}
+`;
+
+export const Main = styled.main`
+  flex-grow: 1;
 `;
