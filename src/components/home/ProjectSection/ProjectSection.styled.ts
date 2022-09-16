44d82c56340012c7b13aa16a2bc1b3a44ae1450b
HaJunRyu <hjr4557@naver.com>
2022-09-16 19:12:06 +0900

    Feat: ProjectSection의 tablet, mobile 반응형 UI를 구현한다

--- src/components/home/ProjectSection/ProjectSection.styled.ts
+++ src/components/home/ProjectSection/ProjectSection.styled.ts
@@ -3,25 +3,59 @@ import styled from '@emotion/styled';
 import { Link } from 'gatsby';
 
 export const ProjectSection = styled.section`
-  margin: 8rem auto 0;
-  padding: 0 2.4rem 11.4rem;
-  max-width: 120rem;
-  text-align: center;
+  ${({ theme }) => css`
+    margin: 8rem auto 0;
+    padding: 0 2.4rem 11.4rem;
+    max-width: 120rem;
+    text-align: center;
+
+    @media (max-width: ${theme.breakPoint.media.tablet}) {
+      padding-bottom: 5.4rem;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.mobile}) {
+      padding-bottom: 4.8rem;
+    }
+  `}
 `;
 
 export const Heading = styled.h2`
   ${({ theme }) => css`
     ${theme.fonts.bold32};
     margin-bottom: 6rem;
+
+    @media (max-width: ${theme.breakPoint.media.tablet}) {
+      margin-bottom: 4rem;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.mobile}) {
+      margin-bottom: 4.8rem;
+    }
   `}
 `;
 
 export const ProjectList = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 36.4rem);
-  grid-template-rows: repeat(2, 43rem);
-  column-gap: 3rem;
-  row-gap: 5.7rem;
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(3, 36.4rem);
+    grid-template-rows: repeat(2, 43rem);
+    column-gap: 3rem;
+    row-gap: 5.7rem;
+
+    @media (max-width: ${theme.breakPoint.media.tablet}) {
+      grid-template-columns: repeat(2, 44.94vw);
+      grid-template-rows: repeat(2, 53.09vw);
+      column-gap: 3.4rem;
+      row-gap: 2.7rem;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.mobile}) {
+      grid-template-columns: 100%;
+      grid-template-rows: repeat(3, 119.44vw);
+      column-gap: 0;
+      row-gap: 2.4rem;
+    }
+  `}
 `;
 
 export const MoreProjectLink = styled(Link)`
@@ -33,5 +67,15 @@ export const MoreProjectLink = styled(Link)`
     background: ${theme.colors.light.black};
     color: ${theme.colors.light.white};
     border-radius: 0.8rem;
+
+    @media (max-width: ${theme.breakPoint.media.tablet}) {
+      margin-top: 5.4rem;
+    }
+
+    @media (max-width: ${theme.breakPoint.media.mobile}) {
+      ${theme.fonts.medium16};
+      margin-top: 4.8rem;
+      padding: 1.4rem 8.75rem;
+    }
   `}
 `;
--- src/components/home/ProjectSection/ProjectSection.tsx
+++ src/components/home/ProjectSection/ProjectSection.tsx
@@ -1,7 +1,15 @@
 import { ArticlePreview } from '@/components/common';
 import { ProjectType } from '@/components/common/ArticlePreview/ArticlePreview';
 import { ROUTES } from '@/constants/route';
+import { ViewPort } from '@/constants/viewport';
+import { useDetectViewport } from '@/hooks';
 import * as Styled from './ProjectSection.styled';
+
+const getNumberOfProjectsPerViewport = (viewportSize: ViewPort) => {
+  if (viewportSize === 'tablet') return 4;
+  if (viewportSize === 'mobile') return 3;
+  return 6;
+};
 
 interface ProjectSectionProps {
   allContentfulProject: { nodes: ProjectType[] };
@@ -9,16 +17,19 @@ interface ProjectSectionProps {
 
 const ProjectSection = ({ allContentfulProject }: ProjectSectionProps) => {
   const { nodes } = allContentfulProject;
+
+  const { viewportSize } = useDetectViewport();
+
+  const numberOfProjects = getNumberOfProjectsPerViewport(viewportSize);
+  const projectList = nodes.slice(0, numberOfProjects);
+
   return (
     <Styled.ProjectSection>
       <Styled.Heading>Featured project</Styled.Heading>
       <Styled.ProjectList>
-        <ArticlePreview article={nodes[0]} />
-        <ArticlePreview article={nodes[0]} />
-        <ArticlePreview article={nodes[0]} />
-        <ArticlePreview article={nodes[0]} />
-        <ArticlePreview article={nodes[0]} />
-        <ArticlePreview article={nodes[0]} />
+        {projectList.map((project) => (
+          <ArticlePreview article={project} />
+        ))}
       </Styled.ProjectList>
       <Styled.MoreProjectLink to={ROUTES.PROJECTS}>더 많은 프로젝트 보기</Styled.MoreProjectLink>
     </Styled.ProjectSection>
