02cbc6f6b8dd2b9906cdcf87f9479c91683e2ea0
HaJunRyu <hjr4557@naver.com>
2022-09-21 14:33:43 +0900

    Refactor: Layout의 max-width와 padding을 제거한다
    HomeLayout이 Layout 컴포넌트와 스타일이 동일해졌으므로 HomeLayout컴포넌트는 제거하고 Layout 컴포넌트로 다시 대체해준다

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,6 +1,5 @@
 export { default as Header } from './Header/Header';
 export { default as ArticlePreviewLg } from './ArticlePreviewLg/ArticlePreviewLg';
 export { default as ArticleSection } from './ArticleSection/ArticleSection';
-export { default as HomeLayout } from './HomeLayout/HomeLayout';
 export { default as WhatWeDo } from './WhatWeDo/WhatWeDo';
 export { default as ProjectSection } from './ProjectSection/ProjectSection';
