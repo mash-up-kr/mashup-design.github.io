3788ef2e54e849822af2b4df60acdb765028e158
HaJunRyu <hjr4557@naver.com>
2022-09-30 00:57:29 +0900

    Feat: article slug를 기반으로 article 상세페이지를 동적 생성한다

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,3 +12,44 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     },
   });
 };
+
+exports.createPages = async ({ graphql, reporter, actions }) => {
+  const { createPage } = actions;
+
+  const articleTemplate = path.resolve(`src/templates/articleTemplate.tsx`);
+
+  const slugResult = await graphql(
+    `
+      query getArticleSlugs {
+        allContentfulArticles {
+          nodes {
+            slug
+          }
+        }
+      }
+    `,
+    { limit: 1000 },
+  );
+
+  if (slugResult.errors) {
+    reporter.panicOnBuild(slugResult.errors);
+    return;
+  }
+
+  if (!slugResult.data) {
+    reporter.panicOnBuild('graphql query result is empty');
+    return;
+  }
+
+  const articleSlugs = slugResult.data.allContentfulArticles.nodes;
+
+  articleSlugs.forEach(({ slug }) => {
+    createPage({
+      path: `/article/${slug}`,
+      component: articleTemplate,
+      context: {
+        slug,
+      },
+    });
+  });
+};
---
+++ src/templates/articleTemplate.tsx
@@ -0,0 +1,5 @@
+const articleTemplate = () => {
+  return <div>articleTemplate</div>;
+};
+
+export default articleTemplate;
